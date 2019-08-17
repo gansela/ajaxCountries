ab210f4eb37093b721ad0a982cf8ea2c4f98573c
gansela <gan.sela@gmail.com>
2019-08-17 16:09:04 +0300

    classic dropdown

--- ex2.js
+++ ex2.js
@@ -1,3 +1,12 @@
+const currencies = {
+    GBP: "https://upload.wikimedia.org/wikipedia/en/b/b9/Bank_of_England_%C2%A350_obverse.jpg",
+    EUR: "https://numismag.com/wp-content/uploads/2018/07/%E2%82%AC100-Banknote-Europa-Serie.jpg",
+    ILS: "https://hskalla.files.wordpress.com/2013/07/d8aed985d8b3d98ad986-d8b4d98ad983d984.jpg?w=584",
+    USD: "https://www.leftovercurrency.com/wp-content/uploads/2016/11/100-american-dollars-banknote-series-1996-obverse-1.jpg",
+    JPY: "https://upload.wikimedia.org/wikipedia/commons/f/ff/1000_yen_Natsume_Soseki.jpg"
+}
+
+
 $(function () {
 
     function getCapital(search, callback) {
@@ -9,24 +18,36 @@ $(function () {
                 callback(data)
             },
             error: function (err) {
-                $("#capital-cards").html("no match")
+                $("#money-cards").html("<h1>No Reasult<h1/>")
             }
         })
     }
-    $(".money").on("click", function () {
+    $(".money").on("change", function () {
+        $("select").css({ "background-image": `url("${currencies[this.value]}")` })
         $("#money-cards").html(`<div class="loader"></div>`)
-        getCapital(this.id, (result) => {
+        getCapital(this.value, (result) => {
             $("#money-cards").html(drawCard(result))
         })
 
     })
+    function init(array) {
+        $(".money").html(array.map(money => {
+            const reasult = `<option value="${money}" id="${money}">${money}</option>`
+            $(`#${money}`).css({ "background-image": `url("${currencies[money]}")` })
+            return reasult
+        }))
+        $("select").prepend("<option selected>Select Currency</option>")
+    }
+
+
+    init(Object.keys(currencies))
 })
 
 function drawCard(array) {
     return array.reduce((stringChain, country) => {
         const { name, capital, flag } = country
         const temp = `<div class="card capital m-1">
-            <img src="${flag}" class="card-img-top">
+            <img src="${flag}" class="card-img-top ">
                 <div class="card-body">
                     <h4 class="card-title">${name}</h4>
                     <h5 class="card-title">${capital}</h5>
@@ -35,3 +56,5 @@ function drawCard(array) {
         return stringChain += temp
     }, "")
 }
+
+
